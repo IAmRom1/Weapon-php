a89eba098097e23c90cc98b0f9970425b87d258f
Romain <90423697+IAmRom1@users.noreply.github.com>
2025-02-06 12:45:05 +0100

    updatee

--- controller/WeaponController.php
+++ controller/WeaponController.php
@@ -6,6 +6,10 @@ class WeaponController {
     public function getWeapons() {
         new Weapon(1, "Epee", "Lame légendaire", 4, ["epee/epee1.png"]);
         new Weapon(2, "Arc", "Arc nul", 2, ["fleau/fleau1.png"]);
+        new Weapon(3, "Fleau", "Fleau de guerre", 3, ["fleau/fleau1.png"]);
+        new Weapon(4, "Hache", "Hache de guerre", 3, ["hache/hache1.png"]);
+        new Weapon(5, "Epee", "Lame légendaire", 4, ["epee/epee1.png"]);
+        new Weapon(6, "Arc", "Arc nul", 2, ["fleau/fleau1.png"]);
         
         return Weapon::$weapons;
     }
